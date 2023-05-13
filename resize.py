a76b59d7891c7688b3cb38f8455fc83b3815518a
cleeem <sp2.clem@gmail.com>
2023-05-13 19:44:55 +0200

    resize to 256 x 256

--- resize.py
+++ resize.py
@@ -1,10 +1,10 @@
 from PIL import Image
 import os
 
-
-for file in os.listdir("weapons"):
-    img = Image.open(f"weapons/{file}")
+key = "image3d"
+for file in os.listdir(f"weapons/{key}"):
+    img = Image.open(f"weapons/{key}/{file}")
     if img.size == (400, 400):
         img2 = img.resize(size=(256,256))
-        img2.save(f"weapons/{file}")
+        img2.save(f"weapons/{key}/{file}")
         print(img2.size, file)
